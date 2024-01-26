68459a0ad58e7af05b2aaf3327eb9a5e64ec6bb3
MiladMk <miladm1995@yahoo.com>
2024-01-26 14:47:38 +0330

    Update Tunneling.sh

--- Tunneling.sh
+++ Tunneling.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-RED="\e[31m"
-GREEN="\e[32m"
-YELLOW="\e[33m"
-BLUE="\e[34m"
-CYAN="\e[36m"
-ENDCOLOR="\e[0m"
-
-echo -e "${YELLOW}************ Tunneling ************"
-echo -e "${RED}-----------------------------------\n"
-echo -e "  ${CYAN}0)Exit"
-echo -e "  1)Enable  Auto Port Forward"
-echo -e "  2)Disable Auto Port Forward"
-
-echo -ne "${GREEN}\nSelect Operation : ${ENDCOLOR}" ;read n
-
-case $n in
-  0) clear;exit;;
-  1) 
-
 echo -e "Enter IP Or Domain"
 read IP ;
 
@@ -42,25 +23,9 @@ echo > /etc/cron.d/port_forward_job
 echo "# AutoPortForward" >>/etc/cron.d/port_forward_job
 echo "@reboot root /etc/Tunneling/port_forward.sh" >>/etc/cron.d/port_forward_job
 echo "0 1 * * * root /etc/Tunneling/reboot.sh" >>/etc/cron.d/port_forward_job
-echo > /etc/cron.d/port_forward_job
 echo -e ""
 echo -e "======================================"
 echo -e ""
 echo -e "      Enable  Auto Port Forward       "
 echo -e ""
 echo -e "======================================"
-exit
-;;
-  2) 
-clear
-echo > /etc/cron.d/port_forward_job
-echo -e ""
-echo -e "======================================"
-echo -e ""
-echo -e "      Disable  Auto Port Forward      "
-echo -e ""
-echo -e "======================================"
-exit
-;;
-  *) echo -e "${RED}\nInvalid Option. ";;
-exit
